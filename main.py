4d2c2ba0162d19d7033ba71cefe62eeaae84d7a8
Jiayu Zhang <jiayuz@uber.com>
2026-07-25 18:47:46 -0400

    01 done

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 class BankAccount:
     def __init__(self):
         self.balance = 0
-    # Implement deposit, withdraw
+
+    def deposite(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("insufficient funds")
+
 
 account = BankAccount()
 import sys
+
 for line in sys.stdin:
     parts = line.strip().split()
     if not parts:
         continue
     cmd = parts[0]
-    # Handle the three commands
+    if cmd == "deposit":
+        account.deposite(int(parts[1]))
+    elif cmd == "withdraw":
+        account.withdraw(int(parts[1]))
+    elif cmd == "balance":
+        print(account.balance)
